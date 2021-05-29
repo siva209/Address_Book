ff672d547d6e69b0e2c5f2c33bb0199643f55917
siva209 <sivanarayanayeruva209@gmail.com>
2021-05-29 22:36:24 +0530

    [siva]Added:Ability to Add multiple AddressBook

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -1,32 +1,32 @@
 package com.addressbook;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class AddressBook {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Problem");
-		AddressMethods address=new AddressMethods();
+	void address_Book(AddressMethods addressMethod) {
 		while (true) {
-			System.out.println("Press 1 : Add Address");
-			System.out.println("Press 2 : Edit Address");
-			System.out.println("Press 3 : Print Address");
-			System.out.println("Press 4 : Delete Address");
+			System.out.println("Press 1 : Add Contact");
+			System.out.println("Press 2 : Edit Contact");
+			System.out.println("Press 3 : Print Contact");
+			System.out.println("Press 4 : Delete Contact");
 			System.out.println("Press 0 : Exit");
 			Scanner scanner = new Scanner(System.in);
 			int choice = scanner.nextInt();
 			switch (choice) {
 			case 1:
-				address.Add();
+				addressMethod.Add();
 				break;
 			case 2:
-				address.Edit();
+				addressMethod.Edit();
 				break;
 			case 3:
-				address.Show();
+				addressMethod.Show();
 				break;
 			case 4:
-				address.Delete();
+				addressMethod.Delete();
 				break;
 			case 0:
 				return;
@@ -34,10 +34,34 @@ public class AddressBook {
 				System.out.println("Please Enter Correct Option");
 			}
 		}
-
-		
 	}
 
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address book");
+		Map<String, AddressMethods> addressbookMap = new HashMap<String, AddressMethods>();
+		String name = null;
+		while (true) {
+
+			System.out.println("Enter the Address book name");
+			Scanner scanner = new Scanner(System.in);
+			name = scanner.nextLine();
+			AddressMethods addressMethods = addressbookMap.get(name);
+			if (addressMethods == null) {
+				addressMethods = new AddressMethods();
+				addressbookMap.put(name, addressMethods);
+			}
+
+			AddressBook book = new AddressBook();
+			book.address_Book(addressMethods);
+			System.out.println("Press 1 : Want to enter in other addressbook");
+			System.out.println("Press any other key to exit");
+			String choise = scanner.nextLine();
+			if (!choise.equals("1"))
+				break;
+		}
+	}
+
+		
 	}
 
 
